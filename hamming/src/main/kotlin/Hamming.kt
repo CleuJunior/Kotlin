1e02669a4535a38de3fb9c34adc655b14bda4787
Cleonildo Jr <cleu.junior@gmail.com>
2023-01-25 11:40:45 -0300

    Hamming

---
+++ hamming/src/main/kotlin/Hamming.kt
@@ -0,0 +1,11 @@
+object Hamming {
+
+    fun compute(leftStrand: String, rightStrand: String): Int {
+        if(leftStrand.length != rightStrand.length)
+            throw IllegalArgumentException("left and right strands must be of equal length")
+
+        return leftStrand.zip(rightStrand)
+                .filter { (leftChar, rightChar) -> leftChar != rightChar  }
+                .size
+    }
+}
